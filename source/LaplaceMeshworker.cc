92a8324c0b7c169940ca692f1f28b976bffc8d13
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2016-03-10 19:06:42 +0100

    deleting comments for rebase.

--- source/LaplaceMeshworker.cc
+++ source/LaplaceMeshworker.cc
@@ -6,18 +6,6 @@ int main ()
   dealii::deallog.attach(logfile);
   dealii::deallog.depth_console (2);
 
-  // Coefficient<2> coeff;
-  // dealii::Point<2> p1(0.5,0.5);
-  // dealii::Point<2> p2(1.0,1.0);
-  // dealii::Point<2> p3(1.0,0.0);
-  // std::vector<dealii::Point<2> > pvector;
-  // pvector.push_back(p1);
-  // pvector.push_back(p2);
-  // pvector.push_back(p3);
-  // std::vector<double> valvector(3);
-  // coeff.value_list(pvector, valvector);
-  // for (unsigned int i=0; i<pvector.size(); ++i)
-  //   dealii::deallog << "value_" << i << " = " << valvector[i] << std::endl;
   MyLaplace<2,true> dgmethod;
   dgmethod.run ();
   return 0;
