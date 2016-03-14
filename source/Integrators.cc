2d1155ecdd7b2c2ef2ca40bf5dbcca57edf66f08
Julius Witte <julius.witte@iwr.uni-heidelberg.de>
2016-03-15 00:29:12 +0100

    nD compile\+run. added RHSintegrator, but not used. wrong peaks -> rhs\?

--- source/Integrators.cc
+++ source/Integrators.cc
@@ -126,9 +126,34 @@ void ResidualIntegrator<dim>::boundary(dealii::MeshWorker::DoFInfo<dim> &dinfo,
      dealii::LocalIntegrators::Laplace::compute_penalty(dinfo,dinfo,deg,deg));
 }
 
+template <int dim>
+RHSIntegrator<dim>::RHSIntegrator()
+{}
+
+template <int dim>
+void RHSIntegrator<dim>::cell(dealii::MeshWorker::DoFInfo<dim> &dinfo, typename dealii::MeshWorker::IntegrationInfo<dim> &info) const
+{
+  const dealii::FEValuesBase<dim> &fe_v = info.fe_values();
+  dealii::Vector<double> &local_vector = dinfo.vector(0).block(0);
+  const std::vector<double> input_vector(local_vector.size(),1.);
+  dealii::LocalIntegrators::L2::L2(local_vector,fe_v,input_vector,1.);
+}
+
+template <int dim>
+void RHSIntegrator<dim>::boundary(dealii::MeshWorker::DoFInfo<dim> &, typename dealii::MeshWorker::IntegrationInfo<dim> &) const
+{}
+template <int dim>
+void RHSIntegrator<dim>::face(dealii::MeshWorker::DoFInfo<dim> &,
+                              dealii::MeshWorker::DoFInfo<dim> &,
+                              typename dealii::MeshWorker::IntegrationInfo<dim> &,
+                              typename dealii::MeshWorker::IntegrationInfo<dim> &) const
+{}
+
 template class MatrixIntegrator<2,false>;
 template class MatrixIntegrator<3,false>;
 template class MatrixIntegrator<2,true>;
 template class MatrixIntegrator<3,true>;
 template class ResidualIntegrator<2>;
 template class ResidualIntegrator<3>;
+template class RHSIntegrator<2>;
+template class RHSIntegrator<3>;
