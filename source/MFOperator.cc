91a6d1d2bd5af74cdae06f3fea77347f99ca8499
Pablo <pablo.lucero@iwr.uni-heidelberg.de>
2017-07-01 21:25:22 +0200

    Bug corrected in multiplicative Schwarz

--- source/MFOperator.cc
+++ source/MFOperator.cc
@@ -95,16 +95,16 @@ void MFOperator<dim,fe_degree,number>::reinit
   ghosted_solution[level] = solution_ ;
   // TODO possibly colorize iterators, assume thread-safety for the moment
   std::vector<std::vector<typename dealii::DoFHandler<dim>::level_cell_iterator> >
-  all_iterators(static_cast<unsigned int>(std::pow(2,dim)));
+    all_iterators(static_cast<unsigned int>(std::pow(2,dim)));
   auto i = 1 ;
   for (auto p=dof_handler->begin_mg(level); p!=dof_handler->end_mg(level); ++p)
     {
       const dealii::types::subdomain_id csid = (p->is_level_cell())
-                                               ? p->level_subdomain_id()
-                                               : p->subdomain_id();
+	? p->level_subdomain_id()
+	: p->subdomain_id();
       if (csid == p->get_triangulation().locally_owned_subdomain())
         {
-          all_iterators[i-1].push_back(p);
+	  all_iterators[i-1].push_back(p);
           i = i % static_cast<unsigned int>(std::pow(2,dim)) ;
           ++i;
         }
