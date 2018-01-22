29d0dba4f6a817e55d4b8bc55a025a1ed4c0ffd9
Stefan Meggendorfer <stefan.meggendorfer@iwr.uni-heidelberg.de>
2018-01-22 16:41:34 +0100

    Space

--- source/Integrators.cc
+++ source/Integrators.cc
@@ -26,6 +26,7 @@ void MatrixIntegrator<dim>::cell(dealii::MeshWorker::DoFInfo<dim> &dinfo,
           ++c ;
         }
       auto &M = dinfo.matrix(b*n_blocks + b).matrix;
+
       LocalIntegrators::Diffusion::cell_matrix<dim>(M,fev,coeffs);
     }
 }
